03ecbad4ce55ff12370a441de591cf8bfdf0f598
DaehoLee <leedaeho1188@gmail.com>
2023-02-08 00:34:14 +0900

    :sparkles: getStaticProps & getStaticPaths 설정 완료

--- pages/post/[slug]/index.tsx
+++ pages/post/[slug]/index.tsx
@@ -1,3 +1,7 @@
+import { POSTS_PATH } from "@/constants";
+import { getPost } from "@/utils/getPost";
+import path from "path";
+import fs from 'fs';
 
 
 function Blog({
@@ -13,13 +17,28 @@ function Blog({
 export default Blog;
 
 export async function getStaticProps({ params }:any) {
+  const { mdxSource, data, content } = await getPost(params.slug)
   return {
     props: {
-
+      mdxSource,
+      frontMatter: data,
+      content
     }
   }
 }
 
+
+
 export function getStaticPaths() {
-  return {}
+  const filePath = path.join(process.cwd(), POSTS_PATH);
+  const postFilePaths = fs.readdirSync(filePath).filter((_path) => /\.mdx?$/.test(_path));
+  const paths = postFilePaths
+    .map((_path) =>  {
+      return {params: { slug: _path.replace(/\.mdx?$/, '')}}
+    })
+
+  return {
+    paths,
+    fallback: false
+  }
 }
--- utils/getPost.ts
+++ utils/getPost.ts
@@ -1,12 +1,21 @@
 import path from "path";
-import fs from 'fs'
+import fs from 'fs';
+import matter from "gray-matter";
+import { serialize } from 'next-mdx-remote/serialize';
+
 import { POSTS_PATH } from "@/constants";
 
 
-export const getPost = (slug: string) => {
+export const getPost = async(slug: string) => {
   const postFilePath = path.join(POSTS_PATH, `${slug}.mdx`)
   const source = fs.readFileSync(postFilePath);
-  const { content, data } = matter()
+  const { content, data } = matter(source);
+  const mdxSource = await serialize(content, {
+    mdxOptions: {
+      remarkPlugins: [],
+      rehypePlugins: [],
+    }
+  });
 
-  return {}
+  return { mdxSource, data, content };
 }
